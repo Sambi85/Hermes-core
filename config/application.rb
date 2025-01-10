045306a2027d87a8719e768d1f764975ffc5480b
Sam Lesser <ineedmoreplates@gmail.com>
2025-01-10 17:24:22 -0600

    add default cors protect from forgery

--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,7 @@ module Hermes
   class Application < Rails::Application
     config.load_defaults 6.1
     config.action_cable.url = ENV['ACTION_CABLE_URL']
+    config.action_controller.default_protect_from_forgery = true
+
   end
 end
