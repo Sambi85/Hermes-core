0a108a3d5a0dd0bf52f6268e3ec8781d621e3f3d
Sam Lesser <ineedmoreplates@gmail.com>
2025-01-10 17:25:30 -0600

    add cors global env to cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,11 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'http://localhost:4000'  # Frontend server address
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete],
-      credentials: false
+if ENV['ENABLE_CORS'] == 'true'
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://localhost:3000'  # Your React app's origin
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :delete, :options, :head],
+        credentials: true
+    end
   end
 end
