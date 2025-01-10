89a935b7520f0e19ffda28c19e59c66b5c5b3b19
Sam Lesser <ineedmoreplates@gmail.com>
2025-01-10 17:33:47 -0600

    disable CSRF for API requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, if: -> { request.format.json? }
+  protect_from_forgery with: :null_session, if: -> { request.format.json? }  # disable CSRF protection for API requests
 
   private
 
